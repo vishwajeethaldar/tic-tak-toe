90fc9d1b8d45b501dd43dd00505abf7b363117aa
Your Name <onlyjeet3@gmail.com>
2024-11-03 05:30:58 +0530

    initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,402 @@
-import Image from "next/image";
+'use client';
+import { useCallback, useEffect, useState } from 'react';
+
+interface InitialStateValuesInterface {
+	box1: string | null;
+	box2: string | null;
+	box3: string | null;
+	box4: string | null;
+	box5: string | null;
+	box6: string | null;
+	box7: string | null;
+	box8: string | null;
+	box9: string | null;
+}
+const initialStateValues: InitialStateValuesInterface = {
+	box1: null,
+	box2: null,
+	box3: null,
+	box4: null,
+	box5: null,
+	box6: null,
+	box7: null,
+	box8: null,
+	box9: null,
+};
+
+const winningCombinations: [
+	keyof InitialStateValuesInterface,
+	keyof InitialStateValuesInterface,
+	keyof InitialStateValuesInterface,
+][] = [
+	['box1', 'box2', 'box3'],
+	['box4', 'box5', 'box6'],
+	['box7', 'box8', 'box9'],
+	['box1', 'box4', 'box7'],
+	['box2', 'box5', 'box8'],
+	['box3', 'box6', 'box9'],
+	['box1', 'box5', 'box9'],
+	['box3', 'box5', 'box7'],
+];
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+	const [boxValues, setBoxValues] =
+		useState<InitialStateValuesInterface>(initialStateValues);
+	const [lastInput, setLastInput] = useState<string | null>(null);
+	const [isGameOver, setIsGameOver] = useState(() => {
+		return Object.values(boxValues)?.filter(Boolean).length === 9;
+	});
+	const [winner, setWinner] = useState<string | null>(null);
+	const [gameLevel, setGameLevel] = useState<number>(0);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+	const handlUpdateValue = useCallback(
+		(boxId: keyof InitialStateValuesInterface) => {
+			if (isGameOver || boxValues[boxId]) {
+				return;
+			}
+
+			// If it's the first move, set the input as "X"
+			if (!lastInput) {
+				setLastInput('X');
+				setBoxValues((prevState) => ({
+					...prevState,
+					[boxId]: 'X',
+				}));
+				return;
+			}
+
+			// Toggle between "X" and "O"
+			const newInput = lastInput === 'X' ? 'O' : 'X';
+			setLastInput(newInput);
+			setBoxValues((prevState) => ({
+				...prevState,
+				[boxId]: newInput,
+			}));
+		},
+		[isGameOver, boxValues, lastInput],
+	);
+
+	const minimax = useCallback(
+		(
+			state: InitialStateValuesInterface,
+			depth: number,
+			isMaximizing: boolean,
+		): number => {
+			const scores: { [key: string]: number } = { X: -1, O: 1, draw: 0 };
+
+			// Check for a terminal state
+			for (const [a, b, c] of winningCombinations) {
+				if (state[a] && state[a] === state[b] && state[a] === state[c]) {
+					return scores[state[a]]; // Return score for the winning player
+				}
+			}
+
+			const isDraw = Object.values(state).every((val) => val !== null);
+			if (isDraw) return scores.draw;
+
+			if (isMaximizing) {
+				let bestScore = -Infinity;
+				for (const boxId of Object.keys(state)) {
+					if (!state[boxId as keyof InitialStateValuesInterface]) {
+						const newState = { ...state, [boxId]: 'O' }; // Computer is 'O'
+						const score = minimax(newState, depth + 1, false);
+						bestScore = Math.max(score, bestScore);
+					}
+				}
+				return bestScore;
+			} else {
+				let bestScore = Infinity;
+				for (const boxId of Object.keys(state)) {
+					if (!state[boxId as keyof InitialStateValuesInterface]) {
+						const newState = { ...state, [boxId]: 'X' }; // Player is 'X'
+						const score = minimax(newState, depth + 1, true);
+						bestScore = Math.min(score, bestScore);
+					}
+				}
+				return bestScore;
+			}
+		},
+		[],
+	);
+
+	// function computerMove() {
+	// 	const emptyBoxes = Object.keys(boxValues).filter(
+	// 		(key) => !boxValues[key as keyof InitialStateValuesInterface],
+	// 	) as (keyof InitialStateValuesInterface)[];
+
+	// 	if (emptyBoxes.length === 0 || isGameOver) return;
+
+	// 	let bestScore = -Infinity;
+	// 	let bestMove: keyof InitialStateValuesInterface | null = null;
+
+	// 	for (const boxId of emptyBoxes) {
+	// 		const newState = { ...boxValues, [boxId]: 'O' }; // Simulate the computer's move
+	// 		const score = minimax(newState, 0, false); // Check the score for the computer's move
+
+	// 		if (score > bestScore) {
+	// 			bestScore = score;
+	// 			bestMove = boxId;
+	// 		}
+	// 	}
+
+	// 	if (bestMove) {
+	// 		handlUpdateValue(bestMove);
+	// 	}
+	// }
+
+	function handleReset() {
+		setIsGameOver(false);
+		setLastInput(null);
+		setWinner(null);
+		setBoxValues(initialStateValues);
+	}
+
+	useEffect(() => {
+		// Function for computer's move
+		function computerMove() {
+			// Get a list of empty boxes
+			const emptyBoxes = Object.keys(boxValues).filter(
+				(key) => !boxValues[key as keyof InitialStateValuesInterface],
+			) as (keyof InitialStateValuesInterface)[];
+
+			// If there are no empty boxes or game is over, return
+			if (emptyBoxes.length === 0 || isGameOver) return;
+
+			let bestScore = -Infinity;
+			let bestMove: keyof InitialStateValuesInterface | null = null;
+
+			for (const boxId of emptyBoxes) {
+				const newState = { ...boxValues, [boxId]: 'O' }; // Simulate the computer's move
+				const score = minimax(newState, 0, false); // Check the score for the computer's move
+
+				if (score > bestScore) {
+					bestScore = score;
+					bestMove = boxId;
+				}
+			}
+
+			if (bestMove && gameLevel === 1) {
+				handlUpdateValue(bestMove);
+			} else {
+				// Select a random box for the computer's move
+				const randomBox =
+					emptyBoxes[Math.floor(Math.random() * emptyBoxes.length)];
+				handlUpdateValue(randomBox);
+			}
+		}
+
+		function checkForWinner() {
+			for (const [a, b, c] of winningCombinations) {
+				if (
+					boxValues[a] &&
+					boxValues[a] === boxValues[b] &&
+					boxValues[a] === boxValues[c]
+				) {
+					setWinner(boxValues[a]);
+					setIsGameOver(true);
+					return true;
+				}
+			}
+			// Draw condition
+			if (Object.values(boxValues).filter(Boolean).length === 9) {
+				setIsGameOver(true);
+				return true;
+			}
+			return false;
+		}
+
+		if (lastInput) {
+			const gameFineshed = checkForWinner();
+			if (!gameFineshed && lastInput === 'X') {
+				setTimeout(() => {
+					computerMove();
+				}, 100);
+			}
+		}
+
+		// function handleTest() {
+		// 	if (
+		// 		boxValues.box1 &&
+		// 		boxValues.box2 &&
+		// 		boxValues.box3 &&
+		// 		boxValues.box1 === boxValues.box2 &&
+		// 		boxValues.box2 === boxValues.box3
+		// 	) {
+		// 		setIsGameOver(true);
+		// 		setWinner(boxValues.box3);
+		// 	} else if (
+		// 		boxValues.box4 &&
+		// 		boxValues.box5 &&
+		// 		boxValues.box6 &&
+		// 		boxValues.box4 === boxValues.box5 &&
+		// 		boxValues.box5 === boxValues.box6
+		// 	) {
+		// 		setIsGameOver(true);
+		// 		setWinner(boxValues.box6);
+		// 	} else if (
+		// 		boxValues.box7 &&
+		// 		boxValues.box8 &&
+		// 		boxValues.box9 &&
+		// 		boxValues.box7 === boxValues.box8 &&
+		// 		boxValues.box8 === boxValues.box9
+		// 	) {
+		// 		setIsGameOver(true);
+		// 		setWinner(boxValues.box9);
+		// 	} else if (
+		// 		boxValues.box1 &&
+		// 		boxValues.box4 &&
+		// 		boxValues.box7 &&
+		// 		boxValues.box1 === boxValues.box4 &&
+		// 		boxValues.box4 === boxValues.box7
+		// 	) {
+		// 		setIsGameOver(true);
+		// 		setWinner(boxValues.box7);
+		// 	} else if (
+		// 		boxValues.box2 &&
+		// 		boxValues.box5 &&
+		// 		boxValues.box8 &&
+		// 		boxValues.box2 === boxValues.box5 &&
+		// 		boxValues.box5 === boxValues.box8
+		// 	) {
+		// 		setIsGameOver(true);
+		// 		setWinner(boxValues.box8);
+		// 	} else if (
+		// 		boxValues.box3 &&
+		// 		boxValues.box6 &&
+		// 		boxValues.box9 &&
+		// 		boxValues.box3 === boxValues.box6 &&
+		// 		boxValues.box6 === boxValues.box9
+		// 	) {
+		// 		setIsGameOver(true);
+		// 		setWinner(boxValues.box9);
+		// 	} else if (
+		// 		boxValues.box1 &&
+		// 		boxValues.box5 &&
+		// 		boxValues.box9 &&
+		// 		boxValues.box1 === boxValues.box5 &&
+		// 		boxValues.box5 === boxValues.box9
+		// 	) {
+		// 		setIsGameOver(true);
+		// 		setWinner(boxValues.box9);
+		// 	} else if (
+		// 		boxValues.box3 &&
+		// 		boxValues.box5 &&
+		// 		boxValues.box7 &&
+		// 		boxValues.box3 === boxValues.box5 &&
+		// 		boxValues.box5 === boxValues.box7
+		// 	) {
+		// 		setIsGameOver(true);
+		// 		setWinner(boxValues.box7);
+		// 	}
+		// }
+	}, [lastInput, boxValues, isGameOver, handlUpdateValue, gameLevel, minimax]);
+
+	return (
+		<div
+			className="flex flex-col items-center justify-center h-screen 
+			bg-neutral-50"
+		>
+			<div className="border text-2xl my-3 px-5 self-start self-start">
+				<span className="bg-slate-50"> Select Level: </span>
+				<select
+					name=""
+					id=""
+					defaultValue={0}
+					onChange={(event) => setGameLevel(Number(event.target.value))}
+				>
+					<option value={0}>Begginer</option>
+					<option value={1}>Advanced</option>
+				</select>
+			</div>
+			<table className="border-collapse xsm:w-5/6 sm:w-80 xsm:h-80 sm:h-80">
+				<caption className="border caption-top text-2xl mb-5">
+					Tic Tac Toe
+				</caption>
+				<caption className="border caption-top text-2xl mb-5">
+					Game Result:{' '}
+					{!lastInput
+						? 'Not Started'
+						: !isGameOver
+							? 'Playing'
+							: !winner
+								? 'Draw'
+								: `${winner} is Winner.`}
+				</caption>
+				<tbody>
+					<tr>
+						<td
+							className="border border-neutral-500 border-4 cursor-pointer font-extrabold text-center 
+							align-middle text-6xl border-t-0 border-l-0 w-1/3 h-1/3"
+							onClick={() => handlUpdateValue('box1')}
+						>
+							{boxValues.box1}
+						</td>
+						<td
+							className="border border-4 border-neutral-500 cursor-pointer font-extrabold text-center 
+							align-middle text-6xl border-t-0 w-1/3 h-1/3"
+							onClick={() => handlUpdateValue('box2')}
+						>
+							{boxValues.box2}
+						</td>
+						<td
+							className="border border-4 border-neutral-500 cursor-pointer font-extrabold text-center
+							align-middle text-6xl border-t-0 border-r-0 w-1/3 h-1/3"
+							onClick={() => handlUpdateValue('box3')}
+						>
+							{boxValues.box3}
+						</td>
+					</tr>
+					<tr>
+						<td
+							className="border border-4 border-neutral-500 cursor-pointer font-extrabold text-center
+							align-middle text-6xl border-l-0 w-1/3 h-1/3"
+							onClick={() => handlUpdateValue('box4')}
+						>
+							{boxValues.box4}
+						</td>
+						<td
+							className="border border-4 border-neutral-500 cursor-pointer font-extrabold text-center
+							align-middle text-6xl w-1/3 h-1/3"
+							onClick={() => handlUpdateValue('box5')}
+						>
+							{boxValues.box5}
+						</td>
+						<td
+							className="border border-4 border-neutral-500 cursor-pointer font-extrabold text-center
+							align-middle text-6xl border-r-0 w-1/3 h-1/3"
+							onClick={() => handlUpdateValue('box6')}
+						>
+							{boxValues.box6}
+						</td>
+					</tr>
+					<tr>
+						<td
+							className="border border-4 border-neutral-500 font-extrabold cursor-pointer text-center
+							align-middle text-6xl border-l-0 border-b-0 w-1/3 h-1/3"
+							onClick={() => handlUpdateValue('box7')}
+						>
+							{boxValues.box7}
+						</td>
+						<td
+							className="border border-4 border-neutral-500 font-extrabold cursor-pointer text-center
+							align-middle text-6xl border-b-0 w-1/3 h-1/3"
+							onClick={() => handlUpdateValue('box8')}
+						>
+							{boxValues.box8}
+						</td>
+						<td
+							className="border border-4 border-neutral-500 font-extrabold cursor-pointer text-center
+							align-middle text-6xl border-r-0 border-b-0 w-1/3 h-1/3"
+							onClick={() => handlUpdateValue('box9')}
+						>
+							{boxValues.box9}
+						</td>
+					</tr>
+				</tbody>
+			</table>
+			<button className="mt-3 border px-5 py-2" onClick={handleReset}>
+				Start Again
+			</button>
+		</div>
+	);
 }
